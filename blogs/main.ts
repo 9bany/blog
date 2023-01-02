99c153c6836d03d7685159b893ae4d736993a2fc
9bany <bany.y0599@gmail.com>
2023-01-02 13:44:07 +0700

    fix: blogs

--- blogs/main.ts
+++ blogs/main.ts
@@ -1,14 +1,14 @@
 import blog from "../blog.tsx";
 
 blog({
-  author: "Dino",
+  author: "rony-y",
   title: "My Blog",
   description: "The blog description.",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
   links: [
-    { title: "bot@deno.com", url: "mailto:bot@deno.com" },
-    { title: "GitHub", url: "https://github.com/denobot" },
+    { title: "bany.y0599", url: "mailto:bany.y0599@gmail.com" },
+    { title: "GitHub", url: "https://github.com/9bany" },
     { title: "Twitter", url: "https://twitter.com/denobot" },
   ],
 });
